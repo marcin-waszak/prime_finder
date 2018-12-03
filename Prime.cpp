0499c74b84cdb3ee7c5cf96860c68fec4735513c
marcin-waszak <m.waszak@stud.elka.pw.edu.pl>
2018-12-03 22:29:26 +0100

    Cosmetical changes in the code

--- Prime.cpp
+++ Prime.cpp
@@ -34,7 +34,7 @@ void Prime::Print() const {
 
 //	for (auto &number : primes_list)
 //		printf("# %lu\n", number);
-	printf("Found Prime: %lu\n", found_);
+	printf("Primes found: %lu\n", found_);
 }
 
 int Prime::Find() {
--- main.cpp
+++ main.cpp
@@ -16,28 +16,30 @@ int main(int argc, char** argv) {
 	number_t a = strtoul(argv[1], nullptr, 0);
 	number_t b = strtoul(argv[2], nullptr, 0);
 
-	fprintf(stderr, "Single:\n");
+	printf("Threads number set to %d.\n", NUM_THREADS);
+
+	printf("[Single] ");
 	tp_t begin = sc_t::now();
 	Prime finder(a, b);
 
 	finder.Find();
 	finder.Print();
 
-	fprintf(stderr, "Posix:\n");
+	printf("[POSIX]  ");
 	tp_t inter = sc_t::now();
 	PrimePosix finder_posix(a, b);
 
 	finder_posix.Find();
 	finder_posix.Print();
 
-	fprintf(stderr, "CPP11:\n");
+	printf("[C++11]  ");
 	tp_t end1 = sc_t::now();
 	PrimeCpp11 finder_cpp11(a, b);
 
 	finder_cpp11.Find();
 	finder_cpp11.Print();
 
-	fprintf(stderr, "OpenMP:\n");
+	printf("[OpenMP] ");
 	tp_t end2 = sc_t::now();
 	PrimeOmp finder_omp(a, b);
 
@@ -51,10 +53,11 @@ int main(int argc, char** argv) {
 	double time_cpp11 = std::chrono::duration_cast<std::chrono::milliseconds>(end2 - end1).count();
 	double time_omp = std::chrono::duration_cast<std::chrono::milliseconds>(end3 - end2).count();
 
-	printf("Single:\t%lf s\n", time_single / 1000);
-	printf("POSIX:\t%lf s\n", time_posix / 1000);
-	printf("CPP11:\t%lf s\n", time_cpp11 / 1000);
-	printf("OpenMP:\t%lf s\n", time_omp / 1000);
+	printf("\nTimes elapsed:\n");
+	printf("Single:\t%.3lf s\n", time_single / 1000);
+	printf("POSIX:\t%.3lf s\n", time_posix / 1000);
+	printf("C++11:\t%.3lf s\n", time_cpp11 / 1000);
+	printf("OpenMP:\t%.3lf s\n", time_omp / 1000);
 
 	return 0;
 }
