454978c32ea295ae64bbb408293d5d370da6d792
BlaszArt <artur@ubuntuvm.com>
2018-12-21 21:55:04 +0100

    Fix bug with receiving data

--- PrimeMpi.cpp
+++ PrimeMpi.cpp
@@ -27,20 +27,22 @@ int PrimeMpi::Find() {
     // allocating memory for worst case where all numbers for task is prime 
     int* taskPrimes = new int[(border_b_ - border_a_) / world_size];
     int primesNum = 0;
-    for (number_t n = border_a_ + world_rank; n <= border_b_; n = n + world_size)
-        if (Check(n))
+    for (number_t n = border_a_ + world_rank; n <= border_b_; n = n + world_size) 
+        if (Check(n)) 
             taskPrimes[primesNum++] = n;
 
     // if not master -> send found primes to master (master rank is 0)
     if (world_rank) {
         MPI_Send(&primesNum, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-		MPI_Send(taskPrimes, primesNum, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	MPI_Send(taskPrimes, primesNum, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
     }
     else { // gather data in master (rank 0)
-        int taskPrimesNum;
+        MPI_Barrier(MPI_COMM_WORLD); // wait for other task to do their job       
+	int taskPrimesNum;
         int* recTaskPrimes; //temp variable for primes recived from other task
         for (int i = 0; i < world_size; i++) {
-            if (world_rank) { // primes from other tasks
+            if (i) { // primes from other tasks
                 MPI_Recv(&taskPrimesNum, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 recTaskPrimes = new int[taskPrimesNum];
                 MPI_Recv(recTaskPrimes, taskPrimesNum, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -53,11 +55,25 @@ int PrimeMpi::Find() {
                     primes_list.push_back(taskPrimes[j]);
             }
         }
-	printf("MPI threads: %d: ", world_size); // TODO: to remove after confirimig flag in cmake works 
         found_ = primes_list.size();
+        Print();
     }
     delete taskPrimes;
 
     MPI_Finalize();
+	return 0;
+}
+
+int main(int argc, char** argv) {
+	if (argc < 3)
+		return 1;
+
+	number_t a = strtoul(argv[1], nullptr, 0);
+	number_t b = strtoul(argv[2], nullptr, 0);
+
+	PrimeMpi finder_mpi(a, b);
+
+	finder_mpi.Find();
+
 	return 0;
 }
