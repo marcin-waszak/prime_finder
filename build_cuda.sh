9e5df0274515e00c9df8d291cd5f6e8f7bb12a52
azygadlo <zygadlo.artur@gmail.com>
2018-12-29 21:39:26 +0100

    added -O3 flag to nvcc

--- build_cuda.sh
+++ build_cuda.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nvcc -std=c++11 -Xcompiler -fopenmp -DNUM_THREADS=8 main_cuda.cpp Prime*.cpp PrimeCuda.cu -o prime_cuda
+nvcc -O3 -std=c++11 -Xcompiler -fopenmp -DNUM_THREADS=8 main_cuda.cpp Prime*.cpp PrimeCuda.cu -o prime_cuda
